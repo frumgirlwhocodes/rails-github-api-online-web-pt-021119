d242d43681aae39bf20e4e5226839989304dfc91
lieby weintraub <liebahinda@gmail.com>
2019-10-23 20:17:46 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,16 @@
 class RepositoriesController < ApplicationController
   
   def index
-    resp=Faraday.get()
+  resp = Faraday.get('https://api.github.com/user') do |req|
+			req.params['access_token'] = session[:token]
+		end
+		@username = JSON.parse(resp.body)['login']
+		resp = Faraday.get('https://api.github.com/user/repos') do |req|
+			req.params['access_token'] = session[:token]
+		end
+		@repos = JSON.parse(resp.body)
   end
-  
+
+end
   
 end
